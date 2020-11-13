7ef24e5be204298fba74f3c52d69b3f4f050a473
surfaffiation <surf2shipaffiliation@gmail.com>
2020-11-13 14:06:40 +0530

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
-
+const git = simpleGit();
 const FILE_PATH = './data.json';
 const DATE = moment().subtract(1,'y').add(1,'d').format();
 const data= {
     date : DATE
 }
 jsonfile.writeFile(FILE_PATH,data, ()=> {
-    simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
+    git.add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
 });
 
 
