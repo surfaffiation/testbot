2fa0b4929d0c99d52c8cc43320fbb3bf81f8f824
surfaffiation <surf2shipaffiliation@gmail.com>
2020-11-13 14:10:19 +0530

    update

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const moment = require('moment');
 const simpleGit = require('simple-git');
 const git = simpleGit();
 const FILE_PATH = './data.json';
-const DATE = moment().subtract(1,'y').add(1,'d').format();
-const data= {
-    date : DATE
+
+const makeCommit = (x,y) =>  {
+    const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'y').format();
+    const data= {
+        date : DATE
+    }
+    jsonfile.writeFile(FILE_PATH,data, ()=> {
+        git.add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
+    });
 }
-jsonfile.writeFile(FILE_PATH,data, ()=> {
-    git.add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
-});
+
+makeCommit(3,3);
+
 
 
 
